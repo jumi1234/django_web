4dcefb2048ceec5b03810368ecf7bc17ef4626cc
dvjm <dvjm@impactstation.kr>
2020-06-26 11:28:49 +0900

    blog post list(index)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', {'posts': posts})
