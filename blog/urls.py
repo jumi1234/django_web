105822d4606542fab138d006a986b8f97b4dff6a
dvjm <dvjm@impactstation.kr>
2020-06-26 11:58:32 +0900

    Post: list view

--- blog/urls.py
+++ blog/urls.py
@@ -17,5 +17,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    # path('', views.index),
+    path('', views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
 
-def index(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/index.html', {'posts': posts})
+class PostList(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
+
+# def index(request):
+#     posts = Post.objects.all()
+#     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
