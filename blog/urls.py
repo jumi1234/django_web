6f4db719d8e41babfcfba93d0978b6c65638d0d6
dvjm <dvjm@impactstation.kr>
2020-06-29 11:27:29 +0900

    post detail add

--- blog/urls.py
+++ blog/urls.py
@@ -18,5 +18,7 @@ from . import views
 
 urlpatterns = [
     # path('', views.index),
+    # path('<int:pk>', views.post_detail),
+    path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
