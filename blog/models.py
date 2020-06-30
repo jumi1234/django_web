e714dc9d76fbff610677a6898ae43cf8196626db
dvjm <dvjm@impactstation.kr>
2020-06-30 11:47:48 +0900

    category in post_list

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     head_image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True)
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True)
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return '{}:{}'.format(self.title, self.author)
