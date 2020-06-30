c00c062fb8883d55b7e17843ff072d65ed645190
dvjm <dvjm@impactstation.kr>
2020-06-30 14:21:54 +0900

    post list by category

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Post, Category
 # Register your models here.
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
 admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
