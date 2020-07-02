a5a2e186d18c4fce915f7a0ba27edfdbaaa99809
jumi1234 <farewellsad89@gmail.com>
2020-07-02 17:37:09 +0900

    blog comment 'created_at', 'modified_at'; delete, edit button

--- blog/models.py
+++ blog/models.py
@@ -57,6 +57,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     text = MarkdownxField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     def get_markdown_content(self):
         return markdown(self.text)
