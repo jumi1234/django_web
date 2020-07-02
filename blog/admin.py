7fc6f0bf10d54310df8824efe90bc852d6d9c5b9
dvjm <dvjm@impactstation.kr>
2020-07-02 09:42:47 +0900

    post deatil page with tag tested

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,9 @@ from .models import Post, Category, Tag
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
 admin.site.register(Post)
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Tag)
+admin.site.register(Tag, TagAdmin)
