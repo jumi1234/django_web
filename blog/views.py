fec22641b93b0d462a1f7c48546cda5989371076
jumi1234 <farewellsad89@gmail.com>
2020-07-03 15:35:19 +0900

    post pagination

--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,10 @@ from django.views.generic import ListView, DetailView, UpdateView, CreateView, D
 
 class PostList(ListView):
     model = Post
+    paginate_by = 5
 
-    def get_queryset(self):
-        return Post.objects.order_by('-created')
+    # def get_queryset(self):
+    #     return Post.objects.order_by('-created')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostList, self).get_context_data(**kwargs)
